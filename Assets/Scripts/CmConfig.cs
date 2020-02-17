6354f698b04271e6be774cb902d8dc7d058e462e
Arif Mertcan Kara <greatadamas@gmail.com>
2020-02-17 16:09:49 +0300

    23
    a

---
+++ Assets/Scripts/CmConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CmConfig : MonoBehaviour
+{
+    private PlayerController targetPlayer;
+    private CinemachineVirtualCamera cmCam;
+
+    private void Start()
+    {
+        cmCam = GetComponent<CinemachineVirtualCamera>();
+        targetPlayer = FindObjectOfType<PlayerController>();
+
+        cmCam.Follow = targetPlayer.transform;
+        cmCam.LookAt = targetPlayer.transform;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,81 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private CharacterController characterController;
+   public float moveSpeed = 10f;
+   public float turnSpeed = 10f;
+   public float jumpForce = 10f;
+   public float gravityScale = 10f;
+   private Vector3 moveDirection;
+   public GunController theGun;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Start()
+   {
+      characterController = GetComponent<CharacterController>();
+   }
+
+
+   private void Update()
+   {
+      MovePlayer();
+      
+      if (Input.GetMouseButtonDown(0))
+      {
+         theGun.isFiring = true;
+      }
+
+      if (Input.GetMouseButtonUp(0))
+      {
+         theGun.isFiring = false;
+      }
+      
+   }
+
+   void MovePlayer()
+   {
+      var yStore = moveDirection.y;
+      var vertical = Input.GetAxisRaw("Vertical");
+      var horizontal = Input.GetAxisRaw("Horizontal");
+
+      moveDirection = (transform.forward * vertical) + (transform.right * horizontal);
+      
+      moveDirection = moveDirection.normalized*moveSpeed;
+      moveDirection.y = yStore;
+
+      if (characterController.isGrounded)
+      {
+         if (Input.GetButtonDown("Jump"))
+         {
+            moveDirection.y = 0f;
+            moveDirection.y = jumpForce;
+         }
+      }
+      
+      moveDirection.y += (Physics.gravity.y * gravityScale*Time.deltaTime);
+      var lookVector = moveDirection;
+      lookVector.y = 0;
+      
+      characterController.Move(moveDirection*Time.deltaTime);
+      
+      if (lookVector.magnitude > 0)
+      {
+         if (vertical >= 0 || Math.Abs(horizontal) > 0)
+         {
+            var newDirection = Quaternion.LookRotation(lookVector);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+         }
+         
+      }
+
+     
+      
+   }
+
+   
+   
 }
